85792a0adde902d19bd9fb3e30d7095387378743
Rahmat Almubarak <rahmatalmubarak35@gmail.com>
2024-01-18 14:13:25 +0700

    perbaikan 5

--- login.php
+++ login.php
@@ -3,85 +3,90 @@ include_once 'includes/config.php';
 
 $config = new Config();
 $db = $config->getConnection();
-	
-if($_POST){
-	
+
+if ($_POST) {
+
 	include_once 'includes/login.inc.php';
 	$login = new Login($db);
 
 	$login->userid = $_POST['username'];
 	$login->passid = md5($_POST['password']);
-	
-	if($login->login()){
+
+	if ($login->login()) {
 		echo "<script>location.href='index.php'</script>";
-	}
-	
-	else{
+	} else {
 		echo "<script>alert('Gagal Total')</script>";
 	}
 }
 ?>
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Penerimaan Beasiswa PPA UIN</title>
 
-    <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<title>Penerimaan Beasiswa PPA UIN</title>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
+	<!-- Bootstrap -->
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
-  </head>
-  <body style="background: #ffffff url(images/back1.jpg) left bottom fixed;">
-  
-	<nav class="navbar navbar-default navbar-static-top">
-	  <div class="container">
-		<!-- Brand and toggle get grouped for better mobile display -->
-		<div class="navbar-header">
-		
-		  <a class="navbar-brand" href="">Penerimaan beasiswa PPA UIN</a>
-		</div>
+</head>
 
-		
-	  </div><!-- /.container-fluid -->
+<body style="background-image:url(images/bg2.jpg); background-size: cover; background-position-y: -50px;">
+
+	<nav class="navbar navbar-default navbar-static-top">
+		<div class="container">
+			<!-- Brand and toggle get grouped for better mobile display -->
+			<div class="navbar-header">
+
+				<a class="navbar-brand" href="">Penerimaan beasiswa PPA UIN</a>
+			</div>
+
+
+		</div><!-- /.container-fluid -->
 	</nav>
-  
-    <div class="container">
-		<div class="row">
-		  <div class="col-xs-12 col-sm-4 col-md-4">&nbsp;</div>
-		  <div class="col-xs-12 col-sm-4 col-md-4">
-		  	
-		  	<div style="margin-top: 100px;" class="panel panel-default"><div class="panel-body">
-		  		<div class="text-center"><h4>Login</h4></div>
-		  		<form method="post">
-				  <div class="form-group">
-				    <label for="InputUsername1">Username</label>
-				    <input type="text" class="form-control" name="username"  id="InputUsername1" placeholder="Nama Pengguna">
-				  </div>
-				  <div class="form-group">
-				    <label for="InputPassword1">Password</label>
-				    <input type="password" class="form-control" name="password" id="InputPassword1" placeholder="Kata Sandi">
-				  </div>
-				  <button type="submit" class="btn btn-primary">Login</button>
-				</form>
-		  	</div></div>
-		  	
-		  </div>
-		  <div class="col-xs-12 col-sm-4 col-md-4">&nbsp;</div>
+
+	<div class="container">
+		<div class="row" >
+			<div class="col-xs-12 col-sm-4 col-md-4">&nbsp;</div>
+			<div class="col-xs-12 col-sm-4 col-md-4">
+
+				<div style="margin-top: 100px;" class="panel panel-default">
+					<div class="panel-body">
+						<div class="text-center">
+							<h4>Login</h4>
+						</div>
+						<form method="post">
+							<div class="form-group">
+								<label for="InputUsername1">Username</label>
+								<input type="text" class="form-control" name="username" id="InputUsername1" placeholder="Nama Pengguna">
+							</div>
+							<div class="form-group">
+								<label for="InputPassword1">Password</label>
+								<input type="password" class="form-control" name="password" id="InputPassword1" placeholder="Kata Sandi">
+							</div>
+							<button type="submit" class="btn btn-primary">Login</button>
+						</form>
+					</div>
+				</div>
+
+			</div>
+			<div class="col-xs-12 col-sm-4 col-md-4">&nbsp;</div>
 		</div>
 	</div>
 
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script src="js/jquery-1.11.3.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script src="js/bootstrap.min.js"></script>
-  </body>
+	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+	<script src="js/jquery-1.11.3.min.js"></script>
+	<!-- Include all compiled plugins (below), or include individual files as needed -->
+	<script src="js/bootstrap.min.js"></script>
+</body>
+
 </html>
